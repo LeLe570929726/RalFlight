884ae536ab5e9f008a8d2b96ed86e3edff660aaa
LeLe570929726 <570929726@qq.com>
2017-01-02 18:21:28 +0800

    Update the copyright information.

---
+++ CMake/FindDX12Directory.cmake
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------------------------------------------------
+# Copyright © 2016 LeLe570929726. All rights reserved.
+# 
+# @Project: RalFlight
+# @License: Licensed under GNU General Public License v3.
+# @Description: A free and open-source flight simulator.
+#
+# @Craete: 2016/12/31 by LeLe570929726
+# ----------------------------------------------------------------------------------------------------
