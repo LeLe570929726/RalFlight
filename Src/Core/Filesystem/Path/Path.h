b9b8e49134207831b724976524f42fa23ab40150
LeLe570929726 <570929726@qq.com>
2017-05-01 17:32:56 +0800

    Rewrite a part of filesystem.

---
+++ Src/Core/Filesystem/Path/Path.h
@@ -0,0 +1,30 @@
+﻿// ----------------------------------------------------------------------------------------------------
+// Copyright © 2016-2017 LeLe570929726. All rights reserved.
+// 
+// @Project: RalFlight
+// @License: Licensed under GNU General Public License v3.
+// @Description: A free and open-source flight simulator.
+//
+// @Craete: 2017/4/28 by LeLe570929726
+// ----------------------------------------------------------------------------------------------------
+#ifndef RALFLIGHT_SRC_CORE_FILESYSTEM_PATH_H
+#define RALFLIGHT_SRC_CORE_FILESYSTEM_PATH_H
+
+#include "../../Global/Macro/Macro.h"
+
+// Core namespace
+namespace Core {
+
+	class RALFLIGHT_API Path {
+	public:
+		Path();
+		Path(const Path &other);
+		~Path() = default;
+
+	private:
+
+	};
+
+}
+
+#endif				// RALFLIGHT_SRC_CORE_FILESYSTEM_PATH_H
