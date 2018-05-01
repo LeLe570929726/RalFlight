728b6e9841bd466a7797856fd8e1566c83998548
LeLe570929726 <570929726@qq.com>
2018-05-01 16:57:26 +0800

    Add vulkan support.

--- 3rdParty/Vulkan/Vulkan.cmake
+++ 3rdParty/Vulkan/Vulkan.cmake
@@ -8,31 +8,38 @@
 # @Create: 2018/3/30 by LeLe570929726
 # ----------------------------------------------------------------------------------------------------
 
-# Header
-if(${RF_OS} STREQUAL "RF_OS_WIN")
-    set(VULKAN_INCLUDE "${PROJECT_SOURCE_DIR}/3rdParty/Vulkan/Windows/Include")
-elseif(${RF_OS} STREQUAL "RF_OS_LINUX")
-    # Todo
+# Config
+include("${PROJECT_SOURCE_DIR}/3rdParty/Vulkan/VulkanConfig.cmake")
+
+# Check config
+if(NOT VULKAN_INCLUDES_PRIVATE)
+    message(FATAL_ERROR "Vulkan config missing. Please check Vulkan config in 3rdParty/Vulkan/VulkanConfig.cmake.")
+endif()
+if(${VULKAN_INCLUDES_PRIVATE} STREQUAL "" OR ${VULKAN_VERSION_PRIVATE} STREQUAL "")
+    message(FATAL_ERROR "Vulkan config missing. Please check Vulkan config in 3rdParty/Vulkan/VulkanConfig.cmake.")
+endif()
+if(${RF_ARCH} STREQUAL "x86")
+    if(${VULKAN_LIBS_PRIVATE_X86} STREQUAL "")
+        message(FATAL_ERROR "Vulkan config missing. Please check Vulkan config in 3rdParty/Vulkan/VulkanConfig.cmake.")
+    endif()
+    set(VULKAN_LIBS_PRIVATE_ROOT ${VULKAN_LIBS_PRIVATE_X86})
+else()
+    if(${VULKAN_LIBS_PRIVATE_X64} STREQUAL "")
+        message(FATAL_ERROR "Vulkan config missing. Please check Vulkan config in 3rdParty/Vulkan/VulkanConfig.cmake.")
+    endif()
+    set(VULKAN_LIBS_PRIVATE_ROOT ${VULKAN_LIBS_PRIVATE_X64})
 endif()
 
 # Version
-if(${RF_OS} STREQUAL "RF_OS_WIN")
-    set(VULKAN_VERSION "1.1.70.1")
-elseif(${RF_OS} STREQUAL "RF_OS_LINUX")
-    # Todo
-endif()
+set(VULKAN_VERSION ${VULKAN_VERSION_PRIVATE})
+
+# Include directories
+set(VULKAN_INCLUDE ${VULKAN_INCLUDES_PRIVATE})
 
 # Libraries
 if(${RF_OS} STREQUAL "RF_OS_WIN")
-    if(${RF_ARCH} STREQUAL "x86")
-        set(DIRECTX_LIBS ${DIRECTX_LIBS}
-            "${PROJECT_SOURCE_DIR}/3rdParty/Vulkan/Windows/Libs/x86/vulkan-1.lib"
-        )
-    elseif(${RF_ARCH} STREQUAL "x64")
-        set(DIRECTX_LIBS ${DIRECTX_LIBS}
-            "${PROJECT_SOURCE_DIR}/3rdParty/Vulkan/Windows/Libs/x64/vulkan-1.lib"
-        )
-    endif()
+    set(QT_LIBS_CORE "${QT_LIBS_PRIVATE_ROOT}/vulkan-1.lib")
 elseif(${RF_OS} STREQUAL "RF_OS_LINUX")
-    # Todo
+    # TODO
+    # set(QT_LIBS_CORE "${QT_LIBS_PRIVATE_ROOT}/libQt5Core.so")
 endif()
