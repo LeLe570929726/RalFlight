7d79b04d3c2d2fd950cd013d87de8546d60c45a7
LeLe570929726 <570929726@qq.com>
2018-05-04 21:16:23 +0800

    Add dx support.

--- 3rdParty/DirectX/DirectX.cmake
+++ 3rdParty/DirectX/DirectX.cmake
@@ -8,23 +8,41 @@
 # @Create: 2018/3/25 by LeLe570929726
 # ----------------------------------------------------------------------------------------------------
 
-# Headers
-set(DIRECTX_INCLUDE "${PROJECT_SOURCE_DIR}/3rdParty/DirectX/Include")
+# Config
+include("${PROJECT_SOURCE_DIR}/3rdParty/DirectX/DirectXConfig.cmake")
+
+# Check config
+if(NOT DIRECTX_INCLUDES_PRIVATE)
+    message(FATAL_ERROR "DirectX config missing. Please check DirectX config in 3rdParty/DirectX/DirectXConfig.cmake.")
+endif()
+if(${DIRECTX_INCLUDES_PRIVATE} STREQUAL "" OR ${DIRECTX_VERSION_PRIVATE} STREQUAL "")
+    message(FATAL_ERROR "DirectX config missing. Please check DirectX config in 3rdParty/DirectX/DirectXConfig.cmake.")
+endif()
+if(${RF_ARCH} STREQUAL "x86")
+    if(${DIRECTX_LIBS_PRIVATE_X86} STREQUAL "")
+        message(FATAL_ERROR "DirectX config missing. Please check DirectX config in 3rdParty/DirectX/DirectXConfig.cmake.")
+    endif()
+    set(QT_LIBS_PRIVATE_ROOT ${DIRECTX_LIBS_PRIVATE_X86})
+else()
+    if(${DIRECTX_LIBS_PRIVATE_X64} STREQUAL "")
+        message(FATAL_ERROR "DirectX config missing. Please check DirectX config in 3rdParty/DirectX/DirectXConfig.cmake.")
+    endif()
+    set(QT_LIBS_PRIVATE_ROOT ${DIRECTX_LIBS_PRIVATE_X64})
+endif()
 
 # Version
-set(DIRECTX_VERSION "10.0.16299.0")
+set(DIRECTX_VERSION ${DIRECTX_VERSION_PRIVATE})
+
+# Include directories
+set(DIRECTX_INCLUDE "${DIRECTX_INCLUDES_PRIVATE}/um"
+    "${DIRECTX_INCLUDES_PRIVATE}/shared")
+set(DIRECTX_INCLUDE_D3D12 "${DIRECTX_INCLUDES_PRIVATE}/um")
+set(DIRECTX_INCLUDE_DXGI "${DIRECTX_INCLUDES_PRIVATE}/shared")
+set(DIRECTX_INCLUDE_XAUDIO2 "${DIRECTX_INCLUDES_PRIVATE}/um")
 
 # Libraries
-if(${RF_ARCH} STREQUAL "x86")
-    set(DIRECTX_LIBS ${DIRECTX_LIBS}
-        "${PROJECT_SOURCE_DIR}/3rdParty/DirectX/Libs/x86/d3d12.lib"
-        "${PROJECT_SOURCE_DIR}/3rdParty/DirectX/Libs/x86/d3dcompiler.lib"
-        "${PROJECT_SOURCE_DIR}/3rdParty/DirectX/Libs/x86/dxgi.lib"
-    )
-elseif(${RF_ARCH} STREQUAL "x64")
-    set(DIRECTX_LIBS ${DIRECTX_LIBS}
-        "${PROJECT_SOURCE_DIR}/3rdParty/DirectX/Libs/x64/d3d12.lib"
-        "${PROJECT_SOURCE_DIR}/3rdParty/DirectX/Libs/x64/d3dcompiler.lib"
-        "${PROJECT_SOURCE_DIR}/3rdParty/DirectX/Libs/x64/dxgi.lib"
-    )
-endif()
+set(DIRECTX_LIBS_D3D12 "${QT_LIBS_PRIVATE_ROOT}/d3d12.lib"
+    "${QT_LIBS_PRIVATE_ROOT}/d3dcompiler.lib")
+set(DIRECTX_LIBS_DXGI "${QT_LIBS_PRIVATE_ROOT}/dxgi.lib")
+set(DIRECTX_LIBS_XAUDIO2 "${QT_LIBS_PRIVATE_ROOT}/xaudio2.lib"
+    "${QT_LIBS_PRIVATE_ROOT}/xaudio2_8.lib")
